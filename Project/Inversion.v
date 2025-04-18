df4ea9484bf2ed87ddeb0ac59b8351878af533f4
Avish Kathpal <kathpal.adil@gmail.com>
2025-04-18 18:48:52 -0400

    add some more inversion lemmas

--- Project/Inversion.v
+++ Project/Inversion.v
@@ -1,8 +1,8 @@
 Require Export SystemMP.Project.Infrastructure.
+Require Export SystemMP.Project.Narrowing.
 Require Import Coq.Program.Equality.
 
 (* Lemma 4.1 *)
-
 Theorem inversion_sub_top : forall E T,
   sub E typ_top T ->
   T = typ_top.
@@ -11,20 +11,35 @@ Proof with eauto.
 Qed.
 
 (* Lemma 4.2 *)
-(* Theorem inversion_sub_fun : forall E S T U, *)
-(*   sub E (typ_arr S T) U -> *)
-(*   U = typ_top \/ *)
-(*   exists S' T', *)
-(*     U = typ_arr S' T' /\ *)
-(*     sub E S' S /\ *)
-(*     (forall z, z `notin` dom E -> sub ((z, bind_val S) :: E) T T'). *)
-(* Proof with eauto using wf_env_weaken_val, wf_typ_weaken_val, sub_weaken_val. *)
-(*   intros. dependent induction H... *)
-(*   - right. *)
-(*     inversion H0; subst. *)
-(*     exists S, T; crush. *)
-(*     eapply sub_refl; rewrite_env (nil ++ (z, bind_val S):: E)... *)
-(*     apply wf_typ_weaken_val... *)
+Theorem inversion_sub_fun : forall E S T U,
+  sub E (typ_arr S T) U ->
+  U = typ_top \/
+  exists L S' T',
+    U = typ_arr S' T' /\
+    sub E S' S /\
+    (forall z, z `notin` L ->
+      sub ((z, bind_val S') :: E) (open_tp T z) (open_tp T' z)).
+Proof with eauto.
+  intros.
+  dependent induction H...
+  - inversion H0; subst...
+    right.
+    exists L, S, T; repeat split...
+  - destruct (IHsub1 S T ltac:(auto)) as [Htop | Hfun]; subst...
+    { apply inversion_sub_top in H0; subst... }
+    destruct Hfun as (L' & S' & T' & H1 & H2 & H3); subst...
+    destruct (IHsub2 S' T' ltac:(auto)) as [Htop' | Hfun']...
+    right.
+    destruct Hfun' as (L'' & S'' & T'' & H4 & H5 & H6); subst...
+    exists (L' `union` L''), S'', T''...
+    repeat split...
+    intros.
+    specialize (H3 z ltac:(fsetdec)).
+    specialize (H6 z ltac:(fsetdec)).
+    eauto using sub_narrow_val.
+  - right.
+    exists L, S2, T2; repeat split...
+Qed.
 
 (* Lemma 4.5 *)
 Theorem inversion_typing_path : forall E (p: pth) T,
